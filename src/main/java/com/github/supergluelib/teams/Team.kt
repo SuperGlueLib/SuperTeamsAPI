ee12a6102ca84008e6c6e065375c6d7b3f8f504d
MLGPenguin <baww123@gmail.com>
2023-09-02 20:03:10 +1000

    Made team variables immutable

--- src/main/java/com/github/supergluelib/teams/Team.kt
+++ src/main/java/com/github/supergluelib/teams/Team.kt
@@ -7,9 +7,10 @@ import java.util.*
 import kotlin.collections.HashSet
 
 open class Team(
-    val uuids: HashSet<UUID> = HashSet(),
+    private val uuids: HashSet<UUID> = HashSet(),
     val name: String? = null,
 ) {
+    private val names: HashSet<String> = uuids.mapNotNull { Bukkit.getOfflinePlayer(it).name }.toHashSet()
     val id = CURRENT_ID++
 
     companion object {
@@ -18,16 +19,27 @@ open class Team(
 
     /**
      * An immutable list of all online players in the team,
-     * for offline players, and marginally better performance under certain circumstances, use [uuids]
+     * for offline players, and marginally better performance under certain circumstances, use [getUUIDs]
      */
     val players: List<Player>
         get() = uuids.mapNotNull { Bukkit.getPlayer(it) }
+    /** @return an immutable list of UUIDs */
+    fun getUUIDs() = uuids.toList()
+    /** @return an immutable list of names of the players in the party */
+    fun getNames() = names.toList()
 
+    /** @return true if the party has no players */
     fun isEmpty() = uuids.isEmpty()
 
-    operator fun plusAssign(player: Player) { uuids.add(player.uniqueId) }
-    fun add(player: Player) = uuids.add(player.uniqueId)
-    fun remove(player: Player) = uuids.remove(player.uniqueId)
+    operator fun plusAssign(player: Player) { add(player) }
+    fun add(player: Player) {
+        uuids.add(player.uniqueId)
+        names.add(player.name)
+    }
+    fun remove(player: Player): Boolean {
+        names.remove(player.name)
+        return uuids.remove(player.uniqueId)
+    }
 
     fun messageAll(msg: String) = players.forEach { it.sendMessage(msg) }
     fun teleportAll(location: Location) = players.forEach { it.teleport(location) }
